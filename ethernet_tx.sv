8eeac9fb3758da74ab3a68eeafe8ff2c7de2f692
Peter Mbua <92553386+Esenju@users.noreply.github.com>
2025-02-10 22:43:35 -0500

    Create ethernet_tx.sv

---
+++ ethernet_tx.sv
@@ -0,0 +1,25 @@
+
+module ehternet_tx(
+  parameter RTP_WIDTH
+)
+  (
+    input logic                   clk, rst_n,
+    ,input logic [RTP_WIDTH-1:0]  rtp_data
+    ,input logic                  rtp_valid 
+    ,input logic                  rtp_valid 
+    ,output logic [RTP_WIDTH-1:0] eth_tx_data,   // Ethernet Output 
+    ,output logic                 eth_tx_valid   // Ethernet TX valid 
+  );
+
+  always_ff @(posedge clk or negedge rst_n) begin 
+    if (!rst_n) begin 
+      eth_tx_valid <= 0;
+    end else begin 
+      if(rtp_valid) begin
+        eth_tx_data <= {16'h0800, rtp_data}; // Ethenet Header + RTP Payload 
+        eth_tx_valid <= 1; 
+      end 
+    end 
+endmodule 
+        
+  
